2d0d1f0877ba80cb4d7ac43f22088298a283c6be
sliiime <kostis.siras@upstreamsystems.com>
2022-07-11 14:01:57 +0300

    Initial commit : Java 18, SpringBoot 2.7.1, Maven 3.8

---
+++ src/main/java/com/skaypal/ebay_clone/EbayCloneApplication.java
@@ -0,0 +1,13 @@
+package com.skaypal.ebay_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbayCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skaypal/ebay_clone/EbayCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skaypal.ebay_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbayCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
