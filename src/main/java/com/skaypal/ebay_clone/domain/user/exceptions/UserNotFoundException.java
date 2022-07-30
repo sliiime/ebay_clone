854f7be54319b5f82e34e2c9942a1860746f1884
Kostis SirAs <kostis.siras@upstreamsystems.com>
2022-07-30 20:19:17 +0300

    Added Exception class in utils and Exceptions in user

---
+++ src/main/java/com/skaypal/ebay_clone/domain/user/exceptions/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package com.skaypal.ebay_clone.domain.user.exceptions;
+
+import com.skaypal.ebay_clone.utils.exceptions.NotFoundException;
+
+public class UserNotFoundException extends NotFoundException {
+
+
+    public UserNotFoundException(String fieldName,String fieldValue){
+        this.message =  String.format("User with %s [%s] was not found",fieldName,fieldName);
+
+    }
+
+
+
+
+}
--- src/main/java/com/skaypal/ebay_clone/utils/Responses.java
+++ src/main/java/com/skaypal/ebay_clone/utils/Responses.java
@@ -1,7 +1,7 @@
 package com.skaypal.ebay_clone.utils;
 
-import org.apache.coyote.Response;
-import org.springframework.dao.DataIntegrityViolationException;
+import com.skaypal.ebay_clone.utils.exceptions.ConflictException;
+import com.skaypal.ebay_clone.utils.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,39 +11,42 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Stream;
 
 @ControllerAdvice
 public class Responses {
 
-    public static <T> ResponseEntity<T> created(String location){
+    public static <T> ResponseEntity<T> created(String location) {
         return ResponseEntity.created(URI.create(location)).build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<String>> methodArgumentNotValidHandler(MethodArgumentNotValidException m){ //Thrown when a constraint is violated by a dto
+    public ResponseEntity<List<String>> methodArgumentNotValidHandler(MethodArgumentNotValidException m) { //Thrown when a constraint is violated by a dto
         List<String> errors = new LinkedList<>();
-        for (FieldError error : m.getBindingResult().getFieldErrors()){
+        for (FieldError error : m.getBindingResult().getFieldErrors()) {
             errors.add(error.getField() + " " + error.getDefaultMessage()); //Needs some improving :D
         }
 
         return ResponseEntity.badRequest().body(errors);
     }
 
-    public static ResponseStatusException notFound(String entity,String fieldName,String fieldValue){
-        return new ResponseStatusException(HttpStatus.NOT_FOUND,entity + " with " + fieldName + " " + fieldValue + " not found");
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> notFoundExceptionHandler(NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
-    
-    public static ResponseStatusException conflict(String entity,List<String> conflicts){
 
-        String conflictFields = "";
-        for (String conflict : conflicts) conflictFields += conflict + " ,";
-        conflictFields = conflictFields.substring(0,conflictFields.length()-2);
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> conflictExceptionHanlder(ConflictException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
 
-        return new ResponseStatusException(HttpStatus.CONFLICT,"There already exists an " +entity +" with this : " + conflictFields);
+    public static ResponseStatusException conflict(String entity, String fieldName, String fieldValue) {
+
+        String article = IsVowel.isVowel(entity.charAt(0)) ? "an" : "a";
+        String message = String.format("There already exists %s %s with % [%]", article, entity, fieldName, fieldValue);
+
+        return new ResponseStatusException(HttpStatus.CONFLICT, message);
 
     }
 
