c2ab796e93992053b93c111667ced3c18a54e61f
Kostis SirAs <kostis.siras@upstreamsystems.com>
2022-09-28 15:02:48 +0300

    Query that returns all users that have participated in a transaction with a specific user

--- src/main/java/com/skaypal/ebay_clone/domain/user/repositories/JPAUserRepository.java
+++ src/main/java/com/skaypal/ebay_clone/domain/user/repositories/JPAUserRepository.java
@@ -2,16 +2,27 @@ package com.skaypal.ebay_clone.domain.user.repositories;
 
 import com.skaypal.ebay_clone.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JPAUserRepository extends JpaRepository<User,Integer> {
 
 
     public Optional<User> findByAfm(String afm);
+
     public Optional<User> findByPhone(String phone);
 
     public Optional<User> findByUsername(String username);
 
     public Optional<User> findByEmail(String email);
+
+    @Query(
+            "SELECT u FROM User u WHERE u.id IN " +
+                    "(SELECT u.id FROM Item i, User u WHERE i.seller.id = ?1 AND i.boughtBy.id = u.id) " +
+                    "OR u.id IN " +
+                    "(SELECT u.id FROM Item i, User u WHERE i.boughtBy.id = ?1 AND u.id = i.seller.id)")
+    public List<User> getTransactionPartners(User user);
 }
+
